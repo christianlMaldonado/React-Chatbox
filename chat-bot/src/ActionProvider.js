22a2f979b1bce335b171912171f2b99bd51a6e84
Christian Maldonado <chris.maldonado99@gmail.com>
2021-07-15 16:47:01 -0700

    Added rest of links and prompts, Bug introduced where only Javascript prompt works

--- chat-bot/src/ActionProvider.js
+++ chat-bot/src/ActionProvider.js
@@ -1,3 +1,4 @@
+
 class ActionProvider {
     constructor(createChatBotMessage, setStateFunc) {
         this.createChatBotMessage = createChatBotMessage;
@@ -9,6 +10,16 @@ class ActionProvider {
         this.updateChatbotState(greetingMessage);
       }
 
+     mainMenu(){
+         const message = this.createChatBotMessage(
+             "Main Menu. Choose from these options:", {
+                 widget: "learningOptions",
+             }
+         );
+
+         this.updateChatbotState(message);
+     };
+
     handleJavascriptList = () => {
         const message = this.createChatBotMessage(
             "Fantastic, I've got the following recources for you on Javascript:",
@@ -19,6 +30,50 @@ class ActionProvider {
 
         this.updateChatbotState(message);
     };
+
+    handleDataVisualizationList = () => {
+        const message = this.createChatBotMessage(
+            "Fantastic, I've got the following recourcees for you on Data Visualization:",
+            {
+                widget: "dataVisualizationLinks",
+            }
+        );
+
+        this.updateChatbotState(message);
+    }
+
+    handleApiList = () => {
+        const message = this.createChatBotMessage(
+            "Fantastic, I've got the following recourcees for you on API's:",
+            {
+                widget: "apiLinks"
+            }
+        );
+
+        this.updateChatbotState(message);
+    }
+
+    handleBlankList = () => {
+        const message = this.createChatBotMessage(
+            "Fantastic, I've got the following recourcees for you on Blank:",
+            {
+                widget: "blankLinks"
+            }
+        );
+
+        this.updateChatbotState(message);
+    }
+
+    handleInterviewPrepList = () => {
+        const message = this.createChatBotMessage(
+            "Fantastic, I've got the following recourcees for you on Interview Prep:",
+            {
+                widget: "interviewPrepLinks"
+            }
+        );
+
+        this.updateChatbotState(message);
+    }
 
     updateChatbotState(message) {
       // NOTE: Function is set in constructoe ans is passsed below  
